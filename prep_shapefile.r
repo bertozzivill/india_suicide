fdda40b1205f614600dc1d444cc37b97593019d3
Amelia Bertozzi-Villa <abertozz@uw.edu>
2015-07-31 16:32:47 +0530

    Prep shapefile that includes all of Jammu & Kashmir and excludes Dadra & Nagar Haveli, Daman & Diu, and Lakshadweep; add mapping code to check new shapefile.

--- prep_shapefile.r
+++ prep_shapefile.r
@@ -16,28 +16,31 @@ rm(list=ls())
 main_dir <- "C:/Users/abertozz/Desktop/practicum/suicide/data/plots/shapefiles/"
 
 #load locations file
-load(paste0(main_dir, "../../clean/causes.rdata"))
-loc <- unique(data[order(state), list(state)])
-loc$merged_id <-rownames(loc)
-#add Telangana (formerly part of Andhra Pradesh)
-loc <- rbind(loc, list("Telangana", 2))
-loc <- loc[order(state)]
-loc$state_id<-as.numeric(rownames(loc))
+loc <-fread(paste0(main_dir, "loc.csv"))
 
 # load shape file
-india_map <- readOGR(paste0(main_dir, "IND_adm1.shp"), layer="IND_adm1")
-#india_map <- spTransform(india_map, CRS("+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"))
-india_map@data <- data.table(state_id = as.numeric(as.character(india_map@data$ID_1)))
+india_map <- readOGR(paste0(main_dir, "download_toofew/india-states-edited-2.shp"), layer="india-states-edited-2")
 
-#manually make adjustments to merge Telangana back into Andhra Pradesh
-india_map@data[state_id==32, state_id:=2]
-india_map <- unionSpatialPolygons(india_map, IDs=india_map@data$state_id)
-india_map <- SpatialPolygonsDataFrame(india_map, data.table(state_id = sapply(india_map@polygons, function(x) x@ID)), F)
-india_map@data[, state_id := as.numeric(as.character(state_id))]
-india_map <- india_map[order(india_map@data$state_id),]
-india_map@data <- merge(india_map@data, loc, by="state_id")
-india_map@data <- india_map@data[order(state_id),]
+statenames <- data.table(india_map@data)
+#sort @data attribute
+india_map@data$unordered_id <- sapply(india_map@polygons, function(x) x@ID)
+india_map@data <- india_map@data[order(india_map$NAME_1),]
+india_map@data$ordered_id <- order(india_map@data$NAME_1)
 
-# save these shapefiles (to be used for actual geographic analysis where distances and position matter)
+#unsort @data
+india_map@data <- india_map@data[order(as.numeric(india_map$unordered_id)),]
+
+#change ID attribute of each @polygons attribute
+idx<-1
+for (ordered_val in india_map@data$ordered_id){
+  print(ordered_val)
+  india_map@polygons[[idx]]@ID <- as.character(ordered_val)
+  print(india_map@polygons[[idx]]@ID)
+  idx <- idx+1
+}
+
+#sort @polygons attribute
+india_map <- india_map[order(india_map@data$ordered_id),]
+
+# save 
 save(india_map, file=paste0(main_dir, "prepped_shapefile.rdata"))
-save(loc, file=paste0(main_dir, "loc.rdata"))
